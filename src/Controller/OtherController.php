43291a3141dad0d38485c86d5b3bc7aefb40c706
khaoula-abaidi <abaidik@gmail.com>
2019-04-19 16:57:16 +0200

    Show Products first commit

--- src/Controller/OtherController.php
+++ src/Controller/OtherController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 use App\Document\Product;
+use App\Repository\ProductRepository;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use MongoDB\Collection;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,11 +19,22 @@ class OtherController
      */
     public function create(DocumentManager $dm):Response{
         $product = new Product();
-        $product->setName('DELL');
-        $product->setPrice('800');
+        $product->setName('HP');
+        $product->setPrice('1200');
         $dm->persist($product);
         $dm->flush();
         return new Response('Created product id '.$product->getName().'hii mongodb');
 
+    }
+
+    /**
+     * @Route("/product/show")
+     * @return Response
+     */
+    public function show(DocumentManager $dm,ProductRepository $repository):Response{
+
+        /** @var Collection $products */
+        $products = $repository->findAll();
+            return new Response('Nous avons '.$products->getCollectionName());
     }
 }
